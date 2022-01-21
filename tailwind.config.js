35d5e2b46755c915fc4b01f0241f88de3e4852cd
Allen <allenwalker1603@gmail.com>
2022-01-21 17:54:20 +0100

    yet another commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E3E8FB'
+      },
+      fontFamily: {
+        sans: [
+          'Alata',
+        ],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
